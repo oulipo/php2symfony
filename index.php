21415be637c84e4d0acb1f1a92b4d6ed4c78a85f
oulipo <laudesj@gmail.com>
2018-06-30 11:52:48 +0200

    isoler le domaine

--- index.php
+++ index.php
@@ -1,23 +1,15 @@
 <?php
 // index.php
-$connection = new PDO("mysql:host=localhost;dbname=blog_db", 'root', '');
+require_once 'model.php';
 
-$result = $connection->query('SELECT id, title FROM posts');
+$posts = get_all_posts();
 
-$posts = array();
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    $posts[] = $row;
-}
-
-$connection = null;
-
-// on inclue le code HTML de présentation
 require 'templates/list.php';
 ?>
 
 <!-- Solutions : 
-    -> 1. isoler "la présentation" : git checkout etape2 
-    2. isoler la logique applicative (domaine) : git checkout etape3
+    1. isoler "la présentation" : git checkout etape2 
+    -> 2. isoler la logique applicative (domaine) : git checkout etape3
     3. isoler le layout : : git checkout etape4
     4. ajouter le page show : : git checkout etape5 
     5. mise en place d'un front-controller : git checkout etape6 
---
+++ model.php
@@ -0,0 +1,29 @@
+<?php
+
+// model.php
+function open_database_connection()
+{
+    $connection = new PDO("mysql:host=localhost;dbname=blog_db", 'root', '');
+
+    return $connection;
+}
+
+function close_database_connection(&$connection)
+{
+    $connection = null;
+}
+
+function get_all_posts()
+{
+    $connection = open_database_connection();
+
+    $result = $connection->query('SELECT id, title FROM posts');
+
+    $posts = array();
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $posts[] = $row;
+    }
+    close_database_connection($connection);
+
+    return $posts;
+}
