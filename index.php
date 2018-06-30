a15856e667f02d9cfe9f1cb5aac8069996acd743
oulipo <laudesj@gmail.com>
2018-06-30 11:57:32 +0200

    isoler le layout

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ require 'templates/list.php';
 
 <!-- Solutions : 
     1. isoler "la présentation" : git checkout etape2 
-    -> 2. isoler la logique applicative (domaine) : git checkout etape3
-    3. isoler le layout : : git checkout etape4
+    2. isoler la logique applicative (domaine) : git checkout etape3
+    -> 3. isoler le layout : : git checkout etape4
     4. ajouter le page show : : git checkout etape5 
     5. mise en place d'un front-controller : git checkout etape6 
     6. ajouter symfony : git checkout symfony -->
--- templates/list.php
+++ templates/list.php
@@ -1,18 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Liste des articles</title>
-    </head>
-    <body>
-        <h1>Liste des articles</h1>
-        <ul>
-            <?php foreach ($posts as $post): ?>
-            <li>
-                <a href="/show.php?id=<?= $post['id'] ?>">
-                    <?= $post['title'] ?>
-                </a>
-            </li>
-            <?php endforeach ?>
-        </ul>
-    </body>
-</html>
+<!-- templates/list.php -->
+<?php $title = 'Liste des articles' ?>
+
+<?php ob_start() ?>
+    <h1>Liste des articles</h1>
+    <ul>
+        <?php foreach ($posts as $post): ?>
+        <li>
+            <a href="/show.php?id=<?= $post['id'] ?>">
+                <?= $post['title'] ?>
+            </a>
+        </li>
+        <?php endforeach ?>
+    </ul>
+<?php $content = ob_get_clean() ?>
+
+<?php include 'layout.php' ?>
