ad9b298790f722f09d7f7eeb7fee57b391e251b7
oulipo <laudesj@gmail.com>
2018-06-30 12:08:28 +0200

    ajouter une page show

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require 'templates/list.php';
 <!-- Solutions : 
     1. isoler "la présentation" : git checkout etape2 
     2. isoler la logique applicative (domaine) : git checkout etape3
-    -> 3. isoler le layout : : git checkout etape4
-    4. ajouter le page show : : git checkout etape5 
+    3. isoler le layout : : git checkout etape4
+    -> 4. ajouter le page show : : git checkout etape5 
     5. mise en place d'un front-controller : git checkout etape6 
     6. ajouter symfony : git checkout symfony -->
